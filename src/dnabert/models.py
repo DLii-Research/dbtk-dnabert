b3867f6baafe878caa537cbb9a4be7d2747dc818
David Ludwig <davidludwigii@gmail.com>
2025-04-04 21:36:30 +0000

    Camera ready

--- src/dnabert/models.py
+++ src/dnabert/models.py
@@ -1,5 +1,5 @@
 from dbtk._utils import export
-from dbtk.nn.models import DbtkModel
+from dbtk.nn.models import BaseModelType, BaseModelClassType, DbtkModel
 from dbtk.nn import layers
 from deprecated import deprecated
 import lightning as L
@@ -8,37 +8,27 @@ from transformers import PretrainedConfig
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from typing import Dict, Optional, Type, Union
+from typing import Dict, Optional, Union
 
-from .datamodules import DnaBertPretrainingDataModule
 from .tokenizers import DnaTokenizer
 
 @export
 class DnaBert(DbtkModel):
     class Config(PretrainedConfig):
-        def __init__(
-            self,
-            kmer=6,
-            kmer_stride=1,
-            normalize_sequences=True,
-            embed_dim: int = 768,
-            num_heads: int = 12,
-            num_layers: int = 6,
-            feedforward_dim: int = 2048,
-            activation: str = "gelu",
-            max_length: int = 250,
-            **kwargs
-        ):
-            super().__init__(**kwargs)
-            self.kmer = kmer
-            self.kmer_stride = kmer_stride
-            self.normalize_sequences = normalize_sequences
-            self.embed_dim = embed_dim
-            self.num_heads = num_heads
-            self.num_layers = num_layers
-            self.feedforward_dim = feedforward_dim
-            self.activation = activation
-            self.max_length = max_length
+        model_type = "dnabert"
+
+        # Model configuration
+        kmer: int = 6
+        kmer_stride: int = 1
+        normalize_sequences: bool = True
+
+        # Transformer configuration
+        embed_dim: int = 768
+        num_heads: int = 12
+        num_layers: int = 6
+        feedforward_dim: int = 2048
+        activation: str = "gelu"
+        max_length: int = 250
 
     config_class = Config
 
@@ -98,20 +88,15 @@ class DnaBert(DbtkModel):
 @export
 class DnaBertForPretraining(DbtkModel):
     class Config(PretrainedConfig):
+        # Enable nesting
         is_composition = True
-        def __init__(
-            self,
-            base: Optional[Union[str, Path, DnaBert.Config, DnaBert]] = None,
-            base_class: Optional[Union[str, Type[DnaBert]]] = "dnabert.models.DnaBert",
-            min_mask_ratio: float = 0.15,
-            max_mask_ratio: float = 0.15,
-            **kwargs
-        ):
-            super().__init__(**kwargs)
-            self.base = base
-            self.base_class = base_class
-            self.min_mask_ratio = min_mask_ratio
-            self.max_mask_ratio = max_mask_ratio
+        model_type = "dnabert"
+
+        # Model configuration
+        base: Optional[BaseModelType[DnaBert]] = None
+        base_class: Optional[BaseModelClassType[DnaBert]] = "dnabert.models.DnaBert"
+        min_mask_ratio: float = 0.15
+        max_mask_ratio: float = 0.15
 
     config_class = Config
     base_model_prefix = "base"
@@ -175,156 +160,32 @@ class DnaBertForPretraining(DbtkModel):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
         return optimizer
 
+    @property
+    def tokenizer(self):
+        return self.base.tokenizer
 
 @export
-class DnaBertForSequenceEmbedding(DbtkModel):
+class DnaBertForEmbedding(DbtkModel):
     class Config(PretrainedConfig):
-        def __init__(
-            self,
-            base: Optional[Union[DnaBert, DnaBert.Config, dict]] = None,
-            **kwargs
-        ):
-            super().__init__(**kwargs)
-            self.base = base if base is not None else DnaBert.Config()
+        # Enable nesting
+        is_composition = True
+
+        # Model configuration
+        base: Optional[BaseModelType[DnaBert]] = None
+        base_class: Optional[BaseModelClassType[DnaBert]] = "dnabert.models.DnaBert"
 
     config_class = Config
     base_model_prefix = "base"
+    sub_models = ["base"]
+
+    base: DnaBert
 
     def __init__(self, config: Optional[Union[Config, dict]] = None):
         super().__init__(config)
-        self.base = self.instantiate_model("base", DnaBert)
 
     def forward(self, kmers: torch.Tensor):
         return self.base(kmers)["class"]
 
-# Legacy
-
-from dbtk._utils import export
-from dbtk.data import tokenizers, vocabularies
-from dbtk.nn import layers
-from dbtk.nn.models import bert
-
-import lightning as L
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from typing import Callable, Optional
-
-@export
-class DnaBertModel(L.LightningModule):
-    def __init__(
-        self,
-        transformer_encoder: layers.TransformerEncoder,
-        kmer: int = 1,
-        kmer_stride: int = 1
-    ):
-        super().__init__()
-        self.transformer_encoder = transformer_encoder
-        self.kmer = kmer
-        self.kmer_stride = kmer_stride
-        self.tokenizer=tokenizers.DnaTokenizer(kmer, kmer_stride)
-        self.vocabulary=bert.BertVocabulary(vocabularies.dna(kmer))
-        self.token_embeddings = nn.Embedding(
-            num_embeddings=len(self.vocabulary),
-            embedding_dim=self.embed_dim,
-            padding_idx=0)
-        self.return_class_embeddings = True
-        self.return_item_embeddings = True
-
-    def forward(
-        self,
-        src: torch.Tensor,
-        average_attention_weights: bool = True,
-        return_attention_weights: bool = False,
-        **kwargs
-    ):
-        # Construct input
-        src = F.pad(src, (1, 0), mode="constant", value=self.vocabulary["[CLS]"])
-        mask = (src == self.vocabulary["[PAD]"])
-        src = self.token_embeddings(src)
-
-        # Pass through transformer encoder
-        output = self.transformer_encoder(
-            src,
-            src_key_padding_mask=mask,
-            average_attention_weights=average_attention_weights,
-            return_attention_weights=return_attention_weights,
-            **kwargs)
-
-        # Construct outputs
-        if isinstance(output, tuple):
-            output, *extra = output
-        else:
-            extra = ()
-        # (class_tokens, output_tokens, extra)
-        # (class_tokens, (output_tokens_a, output_tokens_b), extra)
-        result = ()
-        if self.return_class_embeddings:
-            result += (output.select(-2, 0),)
-        if self.return_item_embeddings:
-            result += (output.narrow(-2, 1, src.shape[-2] - 1),)
-        result += extra
-        if len(result) == 1:
-            return result[0]
-        return result
-
     @property
-    def embed_dim(self):
-        return self.transformer_encoder.embed_dim
-
-    def outputs(self, class_embeddings: Optional[bool] = None, item_embeddings: Optional[bool] = None) -> "BertModel":
-        """
-        Configure what the model should return.
-        """
-        if class_embeddings is not None:
-            self.return_class_embeddings = class_embeddings
-        if item_embeddings is not None:
-            self.return_item_embeddings = item_embeddings
-        return self
-
-
-@export
-class DnaBertPretrainingModel(L.LightningModule):
-    def __init__(self, base: DnaBertModel):
-        super().__init__()
-        self.base = base
-        self.predict_tokens = nn.Linear(
-            self.embed_dim,
-            len(self.base.vocabulary))
-
-    def forward(
-        self,
-        src: torch.Tensor
-    ):
-        return self.base(src)
-
-    def _step(self, mode, batch):
-        src, masked_tokens = batch
-        class_tokens, output = self(src)
-
-        indices = torch.where(src.flatten() == self.base.vocabulary["[MASK]"])
-        predicted = self.predict_tokens(output.flatten(0, -2)[indices])
-        loss = F.cross_entropy(predicted, masked_tokens)
-        num_correct = torch.sum(torch.argmax(predicted, dim=-1) == masked_tokens)
-
-        n = masked_tokens.shape[-1]
-        self.log(f"{mode}/loss", loss, prog_bar=True)
-        self.log(f"{mode}/reconstruction_accuracy", num_correct.float() / n, prog_bar=True)
-        return loss
-
-    def training_step(self, batch):
-        return self._step("train", batch)
-
-    def validation_step(self, batch):
-        return self._step("val", batch)
-
-    def test_step(self, batch):
-        return self._step("test", batch)
-
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=1e-4) # type: ignore
-        # return schedulefree.AdamWScheduleFree(self.parameters(), lr=1e-4)
-
-    @property
-    def embed_dim(self):
-        return self.base.embed_dim
+    def tokenizer(self):
+        return self.base.tokenizer
